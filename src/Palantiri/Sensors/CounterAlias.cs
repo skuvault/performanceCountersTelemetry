f48740d6b33792fecb10ea8c9f88907c9b960682
MaxKits <maxim.kitsenko.r31@gmail.com>
2016-06-26 23:49:51 +0300

    alias added

---
+++ src/Palantiri/Sensors/CounterAlias.cs
@@ -0,0 +1,12 @@
+﻿namespace Palantiri.Sensors
+{
+	public class CounterAlias
+	{
+		public string _alias{ get; private set; }
+
+		public CounterAlias( string alias )
+		{
+			this._alias = alias;
+		}
+	}
+}
