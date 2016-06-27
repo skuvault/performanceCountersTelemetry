6ec5987cab0e351b95e78d6bc972b85b67dc7e2b
MaxKits <maxim.kitsenko.r31@gmail.com>
2016-06-27 14:48:03 +0300

    arguments position removed

--- src/Palantiri.Console/Arguments/Parameters/Counter.cs
+++ src/Palantiri.Console/Arguments/Parameters/Counter.cs
@@ -4,16 +4,16 @@ namespace Palantiri.Console.Arguments.Parameters
 {
 	public class Counter
 	{
-		[ ArgRequired, ArgDescription( "Category" ), ArgShortcut( "c" ), ArgExample( ".NET CLR Memory", "Category name" ), ArgPosition( 1 ) ]
+		[ ArgRequired, ArgDescription( "Category" ), ArgShortcut( "c" ), ArgExample( ".NET CLR Memory", "Category name" ) ]
 		public string Category{ get; set; }
 
-		[ ArgRequired, ArgDescription( "Name" ), ArgShortcut( "n" ), ArgPosition( 2 ) ]
+		[ ArgRequired, ArgDescription( "Name" ), ArgShortcut( "n" ) ]
 		public string Name{ get; set; }
 
-		[ ArgRequired, ArgDescription( "Instance" ), ArgShortcut( "i" ), ArgPosition( 3 ) ]
+		[ ArgRequired, ArgDescription( "Instance" ), ArgShortcut( "i" ) ]
 		public string Instance{ get; set; }
 
-		[ ArgDescription( "Alias" ), ArgShortcut( "a" ), ArgPosition( 3 ) ]
+		[ ArgDescription( "Alias" ), ArgShortcut( "a" ) ]
 		public string Alias{ get; set; }
 	}
 }
