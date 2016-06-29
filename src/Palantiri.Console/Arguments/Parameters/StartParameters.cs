d5d04d1044d971c6f05d0da7a03da4f5ac5868e0
MaxKits <maxim.kitsenko.r31@gmail.com>
2016-06-29 15:00:48 +0300

    code cleaned

--- src/Palantiri.Console/Arguments/Parameters/StartParameters.cs
+++ src/Palantiri.Console/Arguments/Parameters/StartParameters.cs
@@ -4,16 +4,7 @@ namespace Palantiri.Console.Arguments.Parameters
 {
 	public class StartParameters
 	{
-		//[ ArgRequired, ArgDescription( "Counters" ), ArgShortcut( "cs" ), ArgExample( ".NET CLR Memory;_Global_;% Time in GC;devenv.exe;SomeAlias|.NET CLR Memory;_Global_;Allocated Bytes/sec;devenv.exe", "Counters full name and aliases" ) ]
-		//public string CountersList{ get; set; }
-
-		//[ ArgRequired, ArgDescription( "Destinations" ), ArgShortcut( "ds" ), ArgExample( "Console|Telegraf", "Counters destinations" ) ]
-		//public string DestinationsList{ get; set; }
-
-		//[ ArgDefaultValue( ";" ), ArgDescription( "Counters/Destination Separator" ), ArgShortcut( "gs" ), ArgExample( ";", "Counters/Destination Separator" ) ]
-		//public string GlobalSeparator{ get; set; }
-
-		[ArgDescription("JSON file with parameters"), ArgShortcut("p"), ArgExample("someCounters.json", "some counters")]
-		public string Path { get; set; }
+		[ ArgDescription( "JSON file with parameters" ), ArgShortcut( "p" ), ArgExample( "someCounters.json", "some counters" ) ]
+		public string Path{ get; set; }
 	}
 }
