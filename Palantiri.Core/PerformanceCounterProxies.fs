02d238b17d28d8d2a3c2119d8b80d07aae969a68
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-12-12 02:33:03 +0300

    Performance counter helpers added

---
+++ Palantiri.Core/PerformanceCounterProxies.fs
@@ -0,0 +1,11 @@
+﻿module PerformanceCounterProxies
+
+type PerforrmanceCounterProxy( counter:System.Diagnostics.PerformanceCounter, alias:string ) = 
+    let mutable _performanceCounter = counter
+    member this.Alias = alias
+    new(counter:System.Diagnostics.PerformanceCounter) = PerforrmanceCounterProxy(counter, null)
+    
+    member this.Counter
+        with get() = lock this (fun () -> _performanceCounter)
+    member this.ReFresh = () (* lock this ( fun () -> *)
+                                       (* _performanceCounter = PerformanceCounterHelper *)
