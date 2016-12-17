20b1602a11681b3220cd0c4a115262ef8dd12807
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-12-17 22:05:47 +0300

    sensor partially implemented

--- Palantiri.Core/SensorObservable.fs
+++ Palantiri.Core/SensorObservable.fs
@@ -2,13 +2,28 @@
 open System.Collections.Concurrent
 open System.Collections.Generic
 open System.Collections
+open System.Diagnostics
 open System.Threading
 open System.Threading.Tasks
 open Counters
 open PerformanceCounterProxies
-//open SensorObserver
+open SensorObserver
 
-//type ISensorObservable =
-//    abstract member AddObservers: ISensorObserver[] -> unit
-//    abstract member RemoveObserver: ISensorObserver -> unit
-//    abstract member NotifyObservers: ConcurrentDictionary< CounterAlias, CounterValue > -> unit
+type ISensorObservable =
+    abstract member AddObservers : ISensorObserver[]-> unit
+    abstract member RemoveObserver: ISensorObserver -> unit
+    abstract member NotifyObservers: ConcurrentDictionary< CounterAlias, CounterValue > -> unit
+
+type Sensor( periosMs:int, recreationPeriodMs:int, counters:PerforrmanceCounterProxy[]) =
+    let mutable _counters = counters
+    let mutable _periodMs = periosMs
+    let mutable _recreationPeriodMs = recreationPeriodMs
+    let mutable _countersQueue = new ConcurrentQueue< ConcurrentDictionary< string, float > >()
+    let mutable _observers = new List< ISensorObserver >()
+
+    interface ISensorObservable with 
+        member this.AddObservers observers = observers |> Seq.iter _observers.Add
+        member this.RemoveObserver observer = _observers.Remove observer |> ignore
+        member this.NotifyObservers counters = _observers |> Seq.iter (fun o -> o.SendCounters counters)
+    
+    static member GetCounterId (pc:PerformanceCounter) = pc.CategoryName + "_" + pc.CounterName + "_" + pc.InstanceName
