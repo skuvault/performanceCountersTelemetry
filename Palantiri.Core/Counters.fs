8b72e171f65359b1e996016056ace91b84b3ce56
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-12-18 16:21:09 +0300

    counters creators added

--- Palantiri.Core/Counters.fs
+++ Palantiri.Core/Counters.fs
@@ -2,21 +2,27 @@
 
 type CounterAlias( alias:string ) = 
     member this.Alias = alias
+    static member Create alias = new CounterAlias (alias)
 
 type CounterCategory( category:string ) = 
     member this.Category = category
+    static member Create category = new CounterCategory( category )
 
 type CounterInstance( instance:string ) = 
     member this.Instance = instance
+    static member Create instance = new CounterInstance( instance )
 
 type CounterName( name:string ) = 
     member this.Name = name
+    static member Create name = new CounterName( name )
 
 type CounterFullName( name:CounterName, category:CounterCategory, instance:CounterInstance ) = 
     member this.Name = name
     member this.Category = category
     member this.Instance = instance
+    static member Create name category instance = new CounterFullName( name, category, instance )
 
 type CounterValue( dateTime:System.DateTime, value:float ) = 
     member this.Value = dateTime
     member this.DateTime = dateTime
+    static member Create dateTime value = new CounterValue( dateTime, value )
