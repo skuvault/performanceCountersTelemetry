cdc6c9d15f1b6bd750ed71f83fdd4bb80d87fcda
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-12-22 03:05:33 +0300

    packages updated for console f

--- src/Palantiri.ConsoleF/Program.fs
+++ src/Palantiri.ConsoleF/Program.fs
@@ -1,7 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open Serilog
+open PowerArgs
+open System.Reflection
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    Log.Information( "Start Palantiri ver:" + Assembly.GetExecutingAssembly().GetName().Version.ToString() )
     0 // return an integer exit code
