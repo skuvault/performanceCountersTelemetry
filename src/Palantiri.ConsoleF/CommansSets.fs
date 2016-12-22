d076bd21ffa0388ac32d90c37fd4a6c7a694d55c
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-12-22 03:23:29 +0300

    console runner added

---
+++ src/Palantiri.ConsoleF/CommansSets.fs
@@ -0,0 +1,7 @@
+﻿module CommandsSets
+open PowerArgs
+
+[<ArgExceptionBehavior( ArgExceptionPolicy.StandardExceptionHandling )>]
+type Commands() =     
+    [<HelpHook; ArgShortcut( "-?" ); ArgDescription( "Shows this help" )>] member val Help = false
+
--- src/Palantiri.ConsoleF/Program.fs
+++ src/Palantiri.ConsoleF/Program.fs
@@ -2,10 +2,14 @@
 // See the 'F# Tutorial' project for more help.
 open Serilog
 open PowerArgs
+open CommandsSets
 open System.Reflection
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
     Log.Information( "Start Palantiri ver:" + Assembly.GetExecutingAssembly().GetName().Version.ToString() )
+    Args.InvokeAction< Commands >( argv ) |> ignore
+    System.Console.WriteLine( "Press `Enter` to exit" )
+    System.Console.ReadLine() |> ignore
     0 // return an integer exit code
