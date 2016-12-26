a92b11dd9fb102f22a3700fd85ecb63e4c3dc5d0
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-12-27 00:18:00 +0300

    sensor manager added

---
+++ src/Palantiri.Core/SensorManager.fs
@@ -0,0 +1,18 @@
+﻿module SensorManager
+open SensorObservable
+open System.Collections.Generic
+open System.Collections
+open System.Linq
+open Serilog
+
+type SensorManager private () =
+    static let instance = SensorManager()
+    static let Sensors = List< Sensor >()
+    static member GetSingleton = instance
+    member this.AddSensors sensors = Sensors.AddRange<|sensors
+    member this.GetSensorsCount sensors = Sensors.Count
+    member this.GetSensorTask sensors = 
+        try
+            Some<|Sensors.First()
+        with
+            | _ as ex -> Log.Error(ex, "There is no sensor"); None
