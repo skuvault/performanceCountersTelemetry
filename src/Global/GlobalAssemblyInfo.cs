6d3ce1a1a8f67b38a1e5bc2d5a82d66b67180f48
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-12-27 14:37:47 +0300

    solution reconfig

---
+++ src/Global/GlobalAssemblyInfo.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[ assembly : ComVisible( false ) ]
+[ assembly : AssemblyProduct("Palantiri") ]
+[ assembly : AssemblyCompany( "" ) ]
+[ assembly : AssemblyCopyright( "Copyright (C) 2016" ) ]
+[ assembly : AssemblyDescription( "" ) ]
+[ assembly : AssemblyTrademark( "" ) ]
+[ assembly : AssemblyCulture( "" ) ]
+[ assembly : CLSCompliant( false ) ]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+// Keep in track with CA API version
+
+[ assembly : AssemblyVersion( "1.0.*" ) ]
--- src/Palantiri.Console/Properties/AssemblyInfo.cs
+++ src/Palantiri.Console/Properties/AssemblyInfo.cs
@@ -6,34 +6,8 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [ assembly : AssemblyTitle( "PerfCoun.Console" ) ]
-[ assembly : AssemblyDescription( "" ) ]
 [ assembly : AssemblyConfiguration( "" ) ]
-[ assembly : AssemblyCompany( "" ) ]
-[ assembly : AssemblyProduct( "PerfCoun.Console" ) ]
-[ assembly : AssemblyCopyright( "Copyright ©  2016" ) ]
-[ assembly : AssemblyTrademark( "" ) ]
-[ assembly : AssemblyCulture( "" ) ]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[ assembly : ComVisible( false ) ]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [ assembly : Guid( "c5683e45-1c13-46c1-af80-09a266004ca7" ) ]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[ assembly : AssemblyVersion( "1.0.0.4" ) ]
-[ assembly : AssemblyFileVersion( "1.0.0.4" ) ]
